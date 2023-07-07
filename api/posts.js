56bbcbf4e0a157c917cd28094b09e83f88a204e5
KierranyC <kierranyc@outlook.com>
2023-07-06 20:31:10 -0500

    writing POST section

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,7 @@
-const express = require('express')
-const postsRouter = express.Router()
-const { getAllPosts } = require('../db')
+const express = require('express');
+const postsRouter = express.Router();
+const { getAllPosts, createPost } = require('../db');
+const { requireUser } = require('./utils');
 
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
@@ -14,6 +15,42 @@ postsRouter.get('/', async (req, res) => {
   res.send({
     posts
   })
+})
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
+
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {
+    authorId: req.user.id,
+    title,
+    content
+  };
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+    const post = await createPost(postData);
+    // this will create the post and the tags for us
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object 
+    if (post) {
+      res.send({ post });
+    }
+    else {
+      next({ name: 'Error', message: 'Failed to create post.' })
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
 })
 
 module.exports = postsRouter;
